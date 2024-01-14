83dd246a921c3203ef76ba646ddd7563aa90c216
Max <opigez.maxence@gmail.com>
2024-01-14 16:53:52 +0100

    Revert "feature: Add bottom tab navigation with icons"
    This reverts commit faa1192d695d3b40140d2c42951351a38de0fb09.

--- app/nav/_layout.tsx
+++ app/nav/_layout.tsx
@@ -1,54 +1,13 @@
+import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
-import Ionicons from 'react-native-vector-icons/Ionicons';
 
 export default function _layout() {
   return (
-      <Tabs screenOptions={{ 
-          tabBarStyle: { backgroundColor: '#64748B' },
-          tabBarActiveTintColor: '#6EE7B7',
-        }}>
-        <Tabs.Screen
-          name='user'
-          options={{
-            tabBarLabel: "Mon compte",
-            tabBarIcon: ({ focused }) => (
-              <Ionicons
-                name="person"
-                size={24}
-                color={focused ? "#6EE7B7" : "#ECFDF5"}
-              />
-            )
-          }}
-        />
-        <Tabs.Screen
-          name='first_page'
-          options={{
-            tabBarLabel: "Home",
-            tabBarIcon: ({ focused }) => (
-              <Ionicons
-                name="home"
-                size={24}
-                color={focused ? "#6EE7B7" : "#ECFDF5"}
-              />
-            )
-          }}
-        />
-        <Tabs.Screen
-          name='calendar'
-          options={{
-            tabBarLabel: "Calendrier",
-            tabBarIcon: ({ focused }) => (
-              <Ionicons
-                name="calendar"
-                size={24}
-                color={focused ? "#6EE7B7" : "#ECFDF5"}
-              />
-            )
-          }}
-        />
-      </Tabs>
+    <Tabs>
+      <Tabs.Screen name='user' options={{tabBarLabel: "User"}}/>
+      <Tabs.Screen name='first_page' options={{tabBarLabel: "Acceuil"}}/>
+      <Tabs.Screen name='calendar'options={{tabBarLabel: "Calendrier"}}/> 
+    </Tabs>
   )
 }
-
-// Suivre la documentation https://reactnavigation.org/docs/bottom-tab-navigator/#options
