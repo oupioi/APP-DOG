fd9bb001b77ee469e8a43f3fb46e46486bd18e02
Theo Folquin <theo.folquin.dev@gmail.com>
2024-02-07 10:56:12 +0100

    fix

--- app/screens/CalendarScreen.tsx
+++ app/screens/CalendarScreen.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React, { useState } from 'react'
-import { EventService } from '../services/EventService';}
+import { EventService } from '../services/EventService';
 
 import CardEvent from '../components/cardEvent'
 
 export default function Calendar() {
   const [Events, SetEvents ] = useState();
-  const data = getEvents();
+  // const data = getEvents();
   return (
    <CardEvent></CardEvent>
   )
