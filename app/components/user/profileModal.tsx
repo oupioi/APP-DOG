e63d16410fa0eb1cca557fd89cf92b158a75b4bb
Theo Folquin <theo.folquin.dev@gmail.com>
2024-02-09 16:28:27 +0100

    feat: profile form v0

--- app/components/user/profileModal.tsx
+++ app/components/user/profileModal.tsx
@@ -1,167 +1,171 @@
 import React, { useEffect, useState } from 'react';
-import {Text, View, TextInput, Pressable} from 'react-native';
+import {Text, View, TextInput, Pressable, Button} from 'react-native';
 import { globalStyleSheet } from '../../../constants/GlobalStyleSheet';
 import { PersonalInfosResponse } from '../../interfaces/ResponseBodies/PersonalInfosResponse';
-import { ValidationError, bool, number, object, string } from 'yup';
+import { bool, number, object, string } from 'yup';
 import { ModifyUserRequest } from '../../interfaces/RequestBodies/ModifyUserRequest';
-import { SecureStoreTool } from '../../utils/SecureStoreTool';
 import { UserService } from '../../services/UserService';
+import { Formik } from 'formik';
 
 type ProfileProps = {
     data: PersonalInfosResponse,
+    refreshUser: () => Promise<void>
 }
 
-const ProfileModal: React.FC<ProfileProps> = ({data}) => {
+/** @todo Recevoir de "data" les données nécessaires au type ModifyUserRequest à la place ? */
+const ProfileModal: React.FC<ProfileProps> = ({data, refreshUser}) => {
     
     const userService = new UserService();
-    const [userId, setUserId] = useState<string|false>(false);
-    useEffect(() => {
-      const getUserId = async () => {
-        const found = await SecureStoreTool.getItem('user_id');
-        setUserId(found);
-      }
 
-      getUserId();
-    }, [])
-    
-    const [email, setEmail] = useState<string>(data.email);
-    const [pseudo, setPseudo] = useState<string>(data.pseudo);
-    /** @todo Modifier le password seulement s'il est rempli */
-    const [password, setPassword] = useState<string|undefined>(undefined);
-    const [firstName, setFirstName] = useState<string>(data.firstName);
-    /** @todo Ajouter un datepicker */
-    const [lastName, setLastName] = useState<string>(data.lastName);
-    const [birthdate, setBirthdate] = useState<string>(new Date(data.birthdate).toLocaleDateString('fr-FR'));
-    const [notifyFriends, setNotifyFriends] = useState<boolean>(data.notifyFriends);
-    const [address, setAddress] = useState<string>(data.address.address);
-    const [zipCode, setZipCode] = useState<number>(data.address.zipCode);
-    const [city, setCity] = useState<string>(data.address.city);
-
-    const [errorMsg, setErrorMsg] = useState<string|null>(null);
-    const [validationMsg, setValidationMsg] = useState<boolean>(false);
-
-
-    let schema = object<ModifyUserRequest>({
+    let schema = object().shape(
+      {
         email: string().email('Must be a valid email').required('Email is required'),
         pseudo: string().required('Pseudo is required'),
         password: string().min(7, 'Password must be of 7 caracters minimum'),
         firstName: string().required('First name is required'),
         lastName: string().required('Last name is required'),
-        birthdate: string().required('Birthdate is required'),
-        notifyFriends: bool(),
-        address: string(),
-        zipCode: number().required('Zip code is required'),
-        city: string().required('City is required'),
+        // birthdate: string().required('Birthdate is required'),
+        notifyFriends: bool().nullable(),
+        address: object().shape({
+          address: string().nullable(),
+          zipCode: number().required('Zip code is required'),
+          city: string().required('City is required')
+        })
       });
-
-    const handleSubmit = async () => {
-        if (userId) {
-            const requestBody = {
-                id: parseInt(userId),
-                email: email,
-                pseudo: pseudo,
-                password: password,
-                firstName: firstName,
-                lastName: lastName,
-                birthdate: birthdate,
-                notifyFriends: notifyFriends,
-                address: address,
-                zipCode: zipCode,
-                city: city,
-                // @todo Changer ça
-                sex: {
-                    id: 1,
-                    name: 'Homme'
-                }
-            }
-            try {
-                await schema.validate(requestBody);
-            } catch (error) {
-                console.log(error);
-                
-                if (error instanceof ValidationError) {
-                    setErrorMsg(error.errors[0]);
-                    console.log(errorMsg);
-                    return;
-                }
-            }
-
-            try {
-                await userService.updateUserInfos({
-                    id: parseInt(userId),
-                    email: email,
-                    pseudo: pseudo,
-                    password: password,
-                    firstName: firstName,
-                    lastName: lastName,
-                    birthdate: birthdate,
-                    notifyFriends: notifyFriends,
-                    address: {
-                        address: address,
-                        zipCode: zipCode,
-                        city: city
-                    },
-                    // @todo Changer ça
-                    sex: {
-                        id: 1,
-                        name: 'Homme'
-                    }
-                });
-                setValidationMsg(true);
-            } catch (error) {
-                console.log(error.response.data.message);
-            }
-        }
+    const modifyUser = async (values: ModifyUserRequest) => {
+      try {
+        await userService.updateUserInfos(values);
+        refreshUser();
+      } catch (error) {
+        console.log(error);
+      }
     }
 
 // @todo transformer les input container en Components (callback setEmail() par exemple)
-  return ( 
-    <View style={globalStyleSheet.modalForm}>
-        <View style={globalStyleSheet.inputContainer}>
-            <Text style={globalStyleSheet.inputLabel}>Email</Text>
-            <TextInput style={globalStyleSheet.inputForm} onChangeText={setEmail} defaultValue={email}/>
+  return (
+    <Formik validationSchema={schema} initialValues={data as ModifyUserRequest} onSubmit={values => modifyUser(values)}>
+      {({ handleChange, handleBlur, handleSubmit, values, isValid, errors}) => (
+        <View style={{width: '100%', display: 'flex', alignItems: 'center', gap: 15}}>
+        <View style={globalStyleSheet.modalForm}>
+          <View style={globalStyleSheet.inputContainer}>
+            <Text style={globalStyleSheet.inputLabel}>Email: </Text>
+            <TextInput
+              onChangeText={handleChange('email')}
+              onBlur={handleBlur('email')}
+              defaultValue={values.email}
+              style={globalStyleSheet.inputForm}
+            />
+            {errors.email &&
+              <Text style={globalStyleSheet.formError}>{errors.email}</Text>
+            }
+          </View>
+          <View style={globalStyleSheet.inputContainer}>
+            <Text style={globalStyleSheet.inputLabel}>Pseudo: </Text>
+            <TextInput
+              onChangeText={handleChange('pseudo')}
+              onBlur={handleBlur('pseudo')}
+              defaultValue={values.pseudo}
+              style={globalStyleSheet.inputForm}
+            />
+            {errors.pseudo &&
+              <Text style={globalStyleSheet.formError}>{errors.pseudo}</Text>
+            }
+          </View>
+          <View style={globalStyleSheet.inputContainer}>
+            <Text style={globalStyleSheet.inputLabel}>New password: </Text>
+            <TextInput
+              onChangeText={handleChange('password')}
+              onBlur={handleBlur('password')}
+              defaultValue={''}
+              style={globalStyleSheet.inputForm}
+              secureTextEntry={true}
+              autoCorrect={false}
+              autoCapitalize='none'
+              clearTextOnFocus={true}
+            />
+            {errors.password &&
+              <Text style={globalStyleSheet.formError}>{errors.password}</Text>
+            }
+          </View>
+          <View style={globalStyleSheet.inputContainer}>
+            <Text style={globalStyleSheet.inputLabel}>First name: </Text>
+            <TextInput
+              onChangeText={handleChange('firstName')}
+              onBlur={handleBlur('firstName')}
+              defaultValue={data.firstName}
+              style={globalStyleSheet.inputForm}
+            />
+            {errors.firstName &&
+              <Text style={globalStyleSheet.formError}>{errors.firstName}</Text>
+            }
+          </View>
+          <View style={globalStyleSheet.inputContainer}>
+            <Text style={globalStyleSheet.inputLabel}>Last name: </Text>
+            <TextInput
+              onChangeText={handleChange('lastName')}
+              onBlur={handleBlur('lastName')}
+              defaultValue={data.lastName}
+              style={globalStyleSheet.inputForm}
+            />
+            {errors.lastName &&
+              <Text style={globalStyleSheet.formError}>{errors.lastName}</Text>
+            }
+          </View>
+          <View style={globalStyleSheet.inputContainer}>
+            <Text style={globalStyleSheet.inputLabel}>Birthdate: </Text>
+            <TextInput
+              onChangeText={handleChange('birthdate')}
+              onBlur={handleBlur('birthdate')}
+              defaultValue={new Date(data.birthdate).toLocaleDateString('fr-FR')}
+              style={globalStyleSheet.inputForm}
+            />
+          </View>
+          <View style={globalStyleSheet.inputContainer}>
+            <Text style={globalStyleSheet.inputLabel}>Address: </Text>
+            <TextInput
+              onChangeText={handleChange('address.address')}
+              onBlur={handleBlur('address.address')}
+              defaultValue={values.address.address}
+              style={globalStyleSheet.inputForm}
+            />
+            {errors.address?.address &&
+              <Text style={globalStyleSheet.formError}>{errors.address?.address}</Text>
+            }
+          </View>
+          <View style={globalStyleSheet.inputContainer}>
+            <Text style={globalStyleSheet.inputLabel}>Zip code: </Text>
+            <TextInput
+              onChangeText={handleChange('address.zipCode')}
+              onBlur={handleBlur('address.zipCode')}
+              defaultValue={data.address?.zipCode.toString()}
+              style={globalStyleSheet.inputForm}
+            />
+            {errors.address?.zipCode &&
+              <Text style={globalStyleSheet.formError}>{errors.address?.zipCode}</Text>
+            }
+          </View>
+          <View style={globalStyleSheet.inputContainer}>
+            <Text style={globalStyleSheet.inputLabel}>City: </Text>
+            <TextInput
+              onChangeText={handleChange('address.city')}
+              onBlur={handleBlur('address.city')}
+              defaultValue={values.address.city}
+              style={globalStyleSheet.inputForm}
+            />
+            {errors.address?.city &&
+              <Text style={globalStyleSheet.formError}>{errors.address?.city}</Text>
+            }
+          </View>
+          
         </View>
-        <View style={globalStyleSheet.inputContainer}>
-            <Text style={globalStyleSheet.inputLabel}>Pseudo</Text>
-            <TextInput style={globalStyleSheet.inputForm} onChangeText={setPseudo} defaultValue={pseudo}/>
-        </View>
-        <View style={globalStyleSheet.inputContainer}>
-            <Text style={globalStyleSheet.inputLabel}>New password</Text>
-            <TextInput autoCapitalize='none' secureTextEntry={true} autoCorrect={false} style={globalStyleSheet.inputForm} onChangeText={setPassword} defaultValue={undefined}/>
-        </View>
-        <View style={globalStyleSheet.inputContainer}>
-            <Text style={globalStyleSheet.inputLabel}>First name</Text>
-            <TextInput style={globalStyleSheet.inputForm} onChangeText={setFirstName} defaultValue={firstName}/>
-        </View>
-        <View style={globalStyleSheet.inputContainer}>
-            <Text style={globalStyleSheet.inputLabel}>Last name</Text>
-            <TextInput style={globalStyleSheet.inputForm} onChangeText={setLastName} defaultValue={lastName}/>
-        </View>
-        <View style={globalStyleSheet.inputContainer}>
-            <Text style={globalStyleSheet.inputLabel}>Birthdate</Text>
-            <TextInput style={globalStyleSheet.inputForm} onChangeText={setBirthdate} defaultValue={birthdate}/>
-        </View>
-        <View style={globalStyleSheet.inputContainer}>
-            <Text style={globalStyleSheet.inputLabel}>Notify friends</Text>
-            <TextInput style={globalStyleSheet.inputForm} onChangeText={(value) => {setEmail(new Date(value).toString())}} defaultValue={notifyFriends.toString()}/>
-        </View>
-        <View style={globalStyleSheet.inputContainer}>
-            <Text style={globalStyleSheet.inputLabel}>Address</Text>
-            <TextInput style={globalStyleSheet.inputForm} onChangeText={setAddress} defaultValue={address}/>
-        </View>
-        <View style={globalStyleSheet.inputContainer}>
-            <Text style={globalStyleSheet.inputLabel}>Zipcode</Text>
-            <TextInput style={globalStyleSheet.inputForm} onChangeText={(value) => {setZipCode(parseInt(value))}} defaultValue={zipCode?.toString()}/>
-        </View>
-        <View style={globalStyleSheet.inputContainer}>
-            <Text style={globalStyleSheet.inputLabel}>City</Text>
-            <TextInput style={globalStyleSheet.inputForm} onChangeText={setCity} defaultValue={city}/>
-        </View>
-        {errorMsg ? <Text style={globalStyleSheet.formError}>{errorMsg}</Text> : null}
-        {validationMsg ? <Text style={globalStyleSheet.validationMsg}>Modifications applied</Text> : null}
-        <Pressable style={[globalStyleSheet.greenButton, {marginTop: 20}]} onPress={handleSubmit}><Text style={globalStyleSheet.greenButtonText}>Save</Text>
-        </Pressable>
-    </View>
+          {isValid ? 
+            <Pressable style={globalStyleSheet.greenButton} onPress={handleSubmit}>
+              <Text style={globalStyleSheet.greenButtonText}>Save</Text>
+            </Pressable>
+          : null}
+      </View>
+     )}
+    </Formik>
   )
 };
 
