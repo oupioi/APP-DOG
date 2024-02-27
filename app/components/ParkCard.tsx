a42999b7a9e1225703b6da404d051c2a303125cc
florianDesmarchelier <florian.desmarchelier98>
2024-02-27 10:55:41 +0100

    wip: affichage des notes des parcs

--- app/components/ParkCard.tsx
+++ app/components/ParkCard.tsx
@@ -2,18 +2,21 @@ import { View, Text, StyleSheet, Image, ViewStyle } from "react-native";
 import { ParkInterface } from "../interfaces/ParkInterface";
 import { MaterialIcons } from '@expo/vector-icons';
 
-export default function ParkCard(park: ParkInterface){
+export default function ParkCard(park: ParkInterface) {
     return (
-        <View style={styles.container}>
-            <MaterialIcons name="park" size={24} color="black" />
-            <Text>{park.name}</Text>
-            <Text>📍{park.address.address}, {park.address.zipCode} {park.address.city}</Text>
+        <View style={styles.card}>
+            <View style={{ flexDirection: "row", gap: 10 }}>
+                <MaterialIcons name="park" size={24} color="black" />
+                <Text>{park.name}</Text>
+            </View>
+            <Text style={{ marginLeft: 20 }}>📍{park.address.address}, {park.address.zipCode} {park.address.city}</Text>
+            <Text style={{ marginLeft: 20 }} /*todo: afficher la note moyenne du park sous forme d'étoile ⭐*/>Note : ⭐⭐⭐⭐⭐</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container: {
+    card: {
         backgroundColor: "white",
         borderWidth: 2,
         borderBottomWidth: 5,
@@ -23,13 +26,7 @@ const styles = StyleSheet.create({
         paddingHorizontal: 5,
         paddingVertical: 10,
         gap: 5,
-        alignItems: 'center',
         width: 300,
         marginRight: 20
     },
-    logo: {
-        width: 100,
-        height: 100,
-        marginBottom: 10,
-    }
 })
